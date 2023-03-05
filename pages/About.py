c38158ea56e8a5fccfecd3b786f6fdc5adc26927
christopherponsonby <ponsonby.christopher@gmail.com>
2023-03-05 08:24:32 -0600

    Implement About page and html formatting

---
+++ pages/About.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("About")
+
+
+st.title("About Developer")
+st.subheader("Christer02")
+st.write("This app was created by an aspiring <b>python developer<b>.",
+         unsafe_allow_html=True)
